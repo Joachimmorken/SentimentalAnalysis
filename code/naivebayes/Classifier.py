00a1a001c1ce0be07315bbad9fe8d4b2668af9a4
Fabian Johnsen <fabian95@live.no>
2018-03-18 15:01:31 +0100

    testing classifier v1

--- code/naivebayes/Classifier.py
+++ code/naivebayes/Classifier.py
@@ -1,8 +1,12 @@
 import json
 from decimal import Decimal as dec
+import os
 
 #Imports vocabulary with stored probabilities for our classifier
-vocabulary = json.load(open("../../both_words_sorted_fix.bayes"))
+# vocabulary = json.load(open("../../both_words.bayes"))
+
+with open("../../both_words_sorted.bayes", "r", encoding="utf8") as file:
+    vocabulary = json.load(file)
 
 #Splits reviews up into an array of processable words
 def divide_and_conquer(doc):
@@ -18,31 +22,39 @@ def divide_and_conquer(doc):
     return new
 
 
-document = "i truly loved this movie it was great"
-
 # Class probabilities (prior(c))
 
 
 def make_prediction(document):
-    prob_positive = 0.5
-    prob_negative = 0.5
+    prob_positive = dec(0.5)
+    prob_negative = dec(0.5)
     sum = 1
     document = divide_and_conquer(document)
     for word in document:
         if word in vocabulary:
             #Add P(w | y={0,1})
-            prob_positive = dec(prob_positive * vocabulary[word]["posprob"])
-            prob_negative = dec(prob_negative * vocabulary[word]["negprob"])
-        else:
+            prob_positive = dec(prob_positive * dec(vocabulary[word]["posprob"]))
+            prob_negative = dec(prob_negative * dec(vocabulary[word]["negprob"]))
+        #else:
             #Laplace smoothing when word unknown
-            prob_positive = dec(prob_positive * (1/(vocabulary["total_words"]["unique"] + vocabulary["pos:class"]["count"])))
-            prob_negative = dec(prob_negative * (1/(vocabulary["total_words"]["unique"] + vocabulary["neg:class"]["count"])))
+            #"prob_positive = dec(prob_positive * (1/(vocabulary["total_words"]["unique"] + vocabulary["pos:class"]["count"])))
+            #prob_negative = dec(prob_negative * (1/(vocabulary["total_words"]["unique"] + vocabulary["neg:class"]["count"])))
+
     if prob_positive > prob_negative:
         return "positive"
     else:
         return "negative"
 
-make_prediction(document)
+
+poscount = 0
+path = "C:/Users/Fab/OneDrive/Skole/INFO284/1rstGroupAssingment/Data/test/neg"
+for filename in os.listdir(path):
+    f = path + "/" + filename
+    with open(f, "r", encoding="utf8") as doc:
+        poscount += 1 if (make_prediction(doc.read()) == "negative") else 0
+
+print(poscount)
+
 
 #   posProb *= list[classPosProb]
 #   negProb *= list[classNegProb]
