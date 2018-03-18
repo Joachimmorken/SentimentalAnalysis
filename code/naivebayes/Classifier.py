589b44ca10719e352f044cf323da2c5158888b65
Joachim Morken <joknumo@gmail.comm>
2018-03-18 11:58:18 +0100

    classifier working ish

--- code/naivebayes/Classifier.py
+++ code/naivebayes/Classifier.py
@@ -1,23 +1,12 @@
 import json
+import math
 
+vocabulary = json.load(open("../../both_words_sorted_fix.bayes"))
 
-
-
-positive_documents = 12500
-negative_documents = 12500
-total_documents = negative_documents + positive_documents
-posSem = 1
-negSem = 1
-
-probability = json.load(open("../SentimentalAnalysis/both_words.json"))
-vocabulary = json.load(open("../SentimentalAnalysis/both_words.json"))
-
-
-
-classes = [0, 1]  # 0, negative, 1, positive
 
 def divide_and_conquer(doc):
-    exclude = [".", ",", "!", "?", "_", "=", "-", "<br>", "<br/>", "\\", "/", "(", ")", "<br", "br>", "<p>", "<>", "<", ">"]
+    exclude = [".", ",", "!", "?", "_", "=", "-", "<br>", "<br/>", "\\", "/", "(", ")", "<br", "br>", "<p>", "<>", "<",
+               ">"]
     new = []
 
     for w in doc.split():
@@ -25,49 +14,31 @@ def divide_and_conquer(doc):
             w = w.replace(e, "")
         if w is not "":
             new.append(w)
-
     return new
 
 
+document = "i truly loved this movie it was great"
 
-"""""""""
-Eksempel data
-
-class probability
-posProb = 0.8
-negProb = 0.2
-
-document = "I fucking loved this movie, it was great"
+# Class probabilities (prior(c))
 
 
-"""""""""
-
-
-def classifier (document, prior, likelihood):
-    posProb = 1
-    negProb = 1
+def make_prediction(document):
+    prob_positive = 0.5
+    prob_negative = 0.5
+    sum = 1
     document = divide_and_conquer(document)
+    for word in document:
+        if word in vocabulary:
+            prob_positive *= (vocabulary["word"]["posprob"])
+            prob_negative *= (vocabulary["word"]["negprob"])
+        if word not in vocabulary:
+            continue
+    if prob_positive > prob_negative:
+        print("Positive")
+    else:
+        print("Negative")
 
-        #   sum = 0.5  # prior[c]
-        for word in document:
-            if word in vocabulary:
-                posProb *= probability["word"]["posProb"]
-                negProb *= probability["word"]["negProb"]
-        posProb *= 0.5
-        negProb *= 0.5
+make_prediction(document)
 
-        if posProb > negProb:
-            return 1
-        else:
-            return 0
-
-
-    #   posProb *= list[classPosProb]
-    #   negProb *= list[classNegProb]
-
-
-
-
-
-
-
+#   posProb *= list[classPosProb]
+#   negProb *= list[classNegProb]
