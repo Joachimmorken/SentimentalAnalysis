99237981a72d731f84206c32ed646ab054f79d94
Fabian Johnsen <fabian95@live.no>
2018-03-18 13:07:57 +0100

    La til decimal

--- code/naivebayes/Classifier.py
+++ code/naivebayes/Classifier.py
@@ -1,5 +1,5 @@
 import json
-import math
+from decimal import Decimal as dec
 
 #Imports vocabulary with stored probabilities for our classifier
 vocabulary = json.load(open("../../both_words_sorted_fix.bayes"))
@@ -31,16 +31,16 @@ def make_prediction(document):
     for word in document:
         if word in vocabulary:
             #Add P(w | y={0,1})
-            prob_positive *= (vocabulary[word]["posprob"])
-            prob_negative *= (vocabulary[word]["negprob"])
+            prob_positive = dec(prob_positive * vocabulary[word]["posprob"])
+            prob_negative = dec(prob_negative * vocabulary[word]["negprob"])
         else:
             #Laplace smoothing when word unknown
-            prob_positive *= 1/(vocabulary["total_words"]["unique"] + vocabulary["pos:class"]["count"])
-            prob_negative *= 1/(vocabulary["total_words"]["unique"] + vocabulary["neg:class"]["count"])
+            prob_positive = dec(prob_positive * (1/(vocabulary["total_words"]["unique"] + vocabulary["pos:class"]["count"])))
+            prob_negative = dec(prob_negative * (1/(vocabulary["total_words"]["unique"] + vocabulary["neg:class"]["count"])))
     if prob_positive > prob_negative:
-        return 1
+        return "positive"
     else:
-        return 0
+        return "negative"
 
 make_prediction(document)
 
